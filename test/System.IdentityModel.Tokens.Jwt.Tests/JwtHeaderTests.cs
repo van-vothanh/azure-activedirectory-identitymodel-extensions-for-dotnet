3961460f85b12308d78149474599be4c51ef6eca
Maria Furman <mafurman@microsoft.com>
2018-06-06 12:56:31 -0700

    Removed an unnecessary 'using' statement

--- test/System.IdentityModel.Tokens.Jwt.Tests/JwtHeaderTests.cs
+++ test/System.IdentityModel.Tokens.Jwt.Tests/JwtHeaderTests.cs
@@ -25,7 +25,6 @@
 //
 //------------------------------------------------------------------------------
 
-using System.Collections.Generic;
 using Microsoft.IdentityModel.Tests;
 using Microsoft.IdentityModel.Tokens;
 using Xunit;
